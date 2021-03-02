9fd375385a2f1ec0d262fd6b11af58afe2a929c3
Douglas Barraza <dbarraza@acid.cl>
2021-03-02 12:26:31 -0300

    saved commit changes

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent {
+
+  buscar(){
+    console.log("key")
+  }
+
+}
